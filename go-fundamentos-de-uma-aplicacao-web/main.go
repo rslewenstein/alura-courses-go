751ea0d66ac4d8cbd27c250b9d6c36745fb14d96
rslewenstein <rafasoaresjf@gmail.com>
2022-04-05 22:45:05 -0300

    adding struct produtos

--- go-fundamentos-de-uma-aplicacao-web/main.go
+++ go-fundamentos-de-uma-aplicacao-web/main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camisa", Descricao: "Azul, social", Preco: 39, Quantidade: 15},
+		{"Tenis", "Nike air", 150, 6},
+		{"Fone", "Multilaser", 70, 45},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }

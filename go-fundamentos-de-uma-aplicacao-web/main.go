5b8c76ddcba42639b63a2d7994b9644b5b41152d
rslewenstein <rafasoaresjf@gmail.com>
2022-04-05 23:48:40 -0300

    creating conection with the database

--- go-fundamentos-de-uma-aplicacao-web/main.go
+++ go-fundamentos-de-uma-aplicacao-web/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	db, err := sql.Open("mysql", "user:password@/alura_loja")
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -15,6 +26,8 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := conectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
